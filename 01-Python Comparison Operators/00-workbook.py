5e9c9f415a87dbb14b9d22b8765d63aa64c5f2c3
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-15 12:58:11 -0800

    Add notes on comparison operators

---
+++ 01-Python Comparison Operators/00-workbook.py
@@ -0,0 +1,14 @@
+print(2 == 3)
+# comparisons will not change type, 
+print('2' == 2) #false
+
+# Logical Operators, and, or, not
+print(1 < 2 < 3) #works but not ideal
+
+1 < 2 and 2 < 3 #true
+
+'h' == 'h' and 2 == 2 #true
+
+'h' == 'h' or 2 == 3 #true
+
+not(2 == 2) #false
